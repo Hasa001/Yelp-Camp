41f693c1d35ab824a80ad25bdaa63d5852d638b5
Hasa001 <sainiharshit8001@gmail.com>
2024-02-01 00:18:12 +0530

    yelp camp

--- schemas.js
+++ schemas.js
@@ -1,39 +1,8 @@
-const BaseJoi = require('joi');
-const sanitizeHtml = require('sanitize-html');
-
-const extension = function (joi) {
-    return {
-    type: 'string',
-    base: joi.string(),
-    messages: {
-
-        'string.round': '{{#label}} must be a round number',
-    },
-
-    rules: {
-
-        round: {
-
-            validate(value, helpers, args, options) {
-
-                const clean = sanitizeHtml(value, {
-                                        allowedTags: [],
-                                        allowedAttributes: {},
-                                    });
-                                    if (clean !== value) {
-                                        console.log(clean)
-                                        return helpers.error('string.escapeHTML', { value })}
-                                    return clean;
-            }
-        }
-  }
-    }
-}
-const Joi = BaseJoi.extend(extension)
+const Joi = require('joi');
 
 module.exports.campgroundSchema = Joi.object({
     campground: Joi.object({
-        title: Joi.string().required().round(),
+        title: Joi.string().required(),
         price: Joi.number().required().min(0),
         image: Joi.string(),
         location: Joi.string().required(),
